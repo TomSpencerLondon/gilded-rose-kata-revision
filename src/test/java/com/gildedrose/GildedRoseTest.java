333907b0d286321d7d3dc76b2235b59fd28a5344
Tom <thomas.spencer@codurance.com>
2020-07-06 16:05:39 +0100

    Initial commit

---
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GildedRoseTest {
+
+    @Test
+    void reduces_quality_twice_as_fast_as_normal_items() {
+        Item[] items = new Item[] { new Item("Conjured Mana Cake", 3, 6) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake", app.items[0].name);
+        assertEquals(4, app.items[0].quality);
+        assertEquals(2, app.items[0].sellIn);
+    }
+
+}
