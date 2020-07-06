7a79efc7d247e8d60abb22201ddd87807250e579
Tom <thomas.spencer@codurance.com>
2020-07-06 20:19:41 +0100

    UpdateQuality Approval

--- src/test/java/com/gildedrose/GildedRoseApprovals.java
+++ src/test/java/com/gildedrose/GildedRoseApprovals.java
@@ -1,15 +1,24 @@
 package com.gildedrose;
 
 import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
 public class GildedRoseApprovals {
 
   @Test
   void updateQuality() {
-    Item[] items = new Item[] { new Item("foo", 0, 0)};
+    CombinationApprovals.verifyAllCombinations(
+        this::doUpdateQuality,
+        new String[]{"foo"},
+        new Integer[]{0},
+        new Integer[]{0});
+  }
+
+  private String doUpdateQuality(String name, int sellIn, int quality) {
+    Item[] items = new Item[] { new Item(name, sellIn, quality)};
     GildedRose app = new GildedRose(items);
     app.updateQuality();
-    Approvals.verify(app.items[0].name);
+    return app.items[0].toString();
   }
 }
