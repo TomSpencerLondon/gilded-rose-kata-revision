eba0eae47778db9cea952852eab7b734c13afa22
Tom <thomas.spencer@codurance.com>
2020-07-06 16:59:56 +0100

    Refactor brie

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,6 +22,10 @@ class GildedRose {
           break;
         }else if (items[i].name.equals("Aged Brie")) {
           items[i].quality = items[i].quality + 1;
+
+          if (items[i].sellIn < 0 && items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
+          }
         }
       }
 
@@ -62,10 +66,6 @@ class GildedRose {
             }
           } else {
             items[i].quality = items[i].quality - items[i].quality;
-          }
-        } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
           }
         }
       }
